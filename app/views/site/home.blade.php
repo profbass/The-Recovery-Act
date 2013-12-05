b157802c5f9ffbcce41d2b9f9c710bd29d194c4b
profbass <profbass@gmail.com>
2013-12-04 22:25:05 -0700

    Style and layout updates

--- app/views/site/home.blade.php
+++ app/views/site/home.blade.php
@@ -8,7 +8,6 @@
 
 {{-- Content --}}
 @section('content')
-
 <!-- Section #1 -->
 <section id="home" data-speed="10" data-type="background">
    <article>
@@ -30,7 +29,36 @@ Since all we need to do is control the speed of the background images, we’ll u
   </article>
 </section>
 <!-- Section #2 -->
-<section id="about" data-speed="4" data-type="background">
+<section id="news" data-speed="4" data-type="background">
+	<div id="circleLeaf">
+		<span class="fa-stack fa-lg fa-5x">
+		  <i class="fa fa-circle fa-stack-2x "></i>
+		  <i class="fa fa-sun-o fa-stack-1x fa-inverse"></i>
+		</span>
+	</div>
+  <article>
+   	<div class="container">
+   		<div class="row">
+		  <div class="col-md-6">
+		  	<h2>Heading For Things</h2>
+		  	<p>According to the specification for Custom Data Attributes, any attribute that starts with data- will be treated as a storage area for private data. Additionally, this won’t affect the layout or presentation.
+Since all we need to do is control the speed of the background images, we’ll use data-type="background" and data-speed="10" as key attributes to specify the necessary parameters.</p>
+		  	<button class="btn btn-md btn-primary btn-lg">SIGN UP FOR OUR NEWS LETTER</button>
+		  </div>
+		  <div class="col-md-6">
+		  	<h2>Heading For Things</h2>
+		  	<p>According to the specification for Custom Data Attributes, any attribute that starts with data- will be treated as a storage area for private data. Additionally, this won’t affect the layout or presentation.
+Since all we need to do is control the speed of the background images, we’ll use data-type="background" and data-speed="10" as key attributes to specify the necessary parameters.</p>
+		  	<button class="btn btn-md btn-primary btn-lg">SIGN UP FOR OUR NEWS LETTER</button>
+		  </div>
+	 	 </div>
+	 </div>
+  </article>
+</section>
+<!-- Section #3 -->
+<section id="about" data-speed="4" data-type="background"></section>
+<!-- Section #2 -->
+<section id="signup" data-speed="4" data-type="background">
 	<div id="circleLeaf">
 		<span class="fa-stack fa-lg fa-5x">
 		  <i class="fa fa-circle fa-stack-2x "></i>
@@ -67,7 +95,7 @@ Since all we need to do is control the speed of the background images, we’ll u
 	 </div>
   </article>
 </section>
-<!-- Section #2 -->
+<!-- Section #4 -->
 <section id="shows" data-speed="4" data-type="background">
 </section>
 
