e400d41b9c1ce90b102d4cb96e3097bf1536cb7e
profbass <profbass@gmail.com>
2014-07-01 19:43:11 -0600

    Update Database migration update

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -7,7 +7,7 @@ class DatabaseSeeder extends Seeder {
         Eloquent::unguard();
 
         // Add calls to Seeders here
-        $this->call('UsersTableSeeder');
+        //$this->call('UsersTableSeeder');
         $this->call('PostsTableSeeder');
         $this->call('CommentsTableSeeder');
         $this->call('RolesTableSeeder');
